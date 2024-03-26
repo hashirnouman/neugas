972ece65bf1f5f4cca77aad004b8037976948a37
Hashir Nouman <hashirnouman76@gmail.com>
2024-03-26 08:38:24 +0500

    structure setup

--- apps/frontend/components/layout/Sidebar.tsx
+++ apps/frontend/components/layout/Sidebar.tsx
@@ -1,13 +1,13 @@
 const Sidebar = () => {
-  return (
-    <aside className='sticky left-0 h-screen max-w-[252px] overflow-y-auto bg-white px-[22px] pt-[32px]'>
-      <img src='/images/Logo.png' alt='logo' className='mb-[60px]' />
-      <div className='flex h-[44px] w-full   gap-[12px] rounded-[10px] bg-[#F5F5F7] px-[20px] py-[10px] text-secondary-default '>
-        <img src='/icons/sidebar/category.png' alt='' />
-        <p className='font-[600]'>Overview</p>
-      </div>
-    </aside>
-  );
+	return (
+		<aside className="sticky left-0 h-screen max-w-[252px] overflow-y-auto bg-white px-[22px] pt-[32px]">
+			<img src="/images/Logo.png" alt="logo" className="mb-[60px]" />
+			<div className="flex h-[44px] w-full   gap-[12px] rounded-[10px] bg-[#F5F5F7] px-[20px] py-[10px] text-secondary-default ">
+				<img src="/icons/sidebar/category.png" alt="" />
+				<p className="font-[600]">Overview</p>
+			</div>
+		</aside>
+	);
 };
 
 export default Sidebar;
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,60 +1,73 @@
-'use client';
-import type { ReactNode } from 'react';
+"use client";
+import type { ReactNode } from "react";
 import {
-  Button as AriaButton,
-  type ButtonProps,
-  type PressEvent,
-} from 'react-aria-components';
+	Button as AriaButton,
+	type ButtonProps,
+	type PressEvent,
+} from "react-aria-components";
 
 // Define a type for valid variants
-type Variant = 'primary' | 'warning' | 'success' | 'error' | 'info' | 'secondary';
+type Variant =
+	| "primary"
+	| "warning"
+	| "success"
+	| "error"
+	| "info"
+	| "secondary";
 
 interface IButtonProps extends ButtonProps {
-  onClick?: (e: PressEvent) => void;
-  children: ReactNode;
-  variant?: Variant | '';
+	onClick?: (e: PressEvent) => void;
+	children: ReactNode;
+	variant?: Variant | "";
 }
 
-const Button = (props: IButtonProps) => {
-  const { onClick, variant, children, isDisabled, ...rest } = props;
+export const Button = (props: IButtonProps) => {
+	const { onClick, variant, children, isDisabled, ...rest } = props;
 
-  let styleClass = '';
-  switch (variant) {
-    case 'primary':
-      styleClass = 'bg-primary-default hover:bg-primary-dark-600 active:bg-primary-dark-700 active:scale-[95%]';
-      break;
-    case 'warning':
-      styleClass = 'bg-warning-default hover:bg-warning-dark-600 active:bg-warning-dark-700 active:scale-[95%]';
-      break;
-    case 'success':
-      styleClass = 'bg-success-default hover:bg-success-dark-600 active:bg-success-dark-700 active:scale-[95%]';
-      break;
-    case 'error':
-      styleClass = 'bg-error-default hover:bg-error-dark-600 active:bg-error-dark-700 active:scale-[95%]';
-      break;
-    case 'info':
-      styleClass = 'bg-info-default hover:bg-info-dark-600 active:bg-info-dark-700 active:scale-[95%]';
-      break;
-    case 'secondary':
-      styleClass = 'bg-secondary-default hover:bg-secondary-dark-600 active:bg-secondary-dark-700 active:scale-[95%]';
-      break;
-    default:
-      // Default to primary if variant is not provided or not recognized
-      styleClass = 'bg-primary-default hover:bg-primary-dark-600 active:bg-primary-dark-700';
-  }
+	let styleClass = "";
+	switch (variant) {
+		case "primary":
+			styleClass =
+				"bg-primary-default hover:bg-primary-dark-600 active:bg-primary-dark-700 active:scale-[95%]";
+			break;
+		case "warning":
+			styleClass =
+				"bg-warning-default hover:bg-warning-dark-600 active:bg-warning-dark-700 active:scale-[95%]";
+			break;
+		case "success":
+			styleClass =
+				"bg-success-default hover:bg-success-dark-600 active:bg-success-dark-700 active:scale-[95%]";
+			break;
+		case "error":
+			styleClass =
+				"bg-error-default hover:bg-error-dark-600 active:bg-error-dark-700 active:scale-[95%]";
+			break;
+		case "info":
+			styleClass =
+				"bg-info-default hover:bg-info-dark-600 active:bg-info-dark-700 active:scale-[95%]";
+			break;
+		case "secondary":
+			styleClass =
+				"bg-secondary-default hover:bg-secondary-dark-600 active:bg-secondary-dark-700 active:scale-[95%]";
+			break;
+		default:
+			// Default to primary if variant is not provided or not recognized
+			styleClass =
+				"bg-primary-default hover:bg-primary-dark-600 active:bg-primary-dark-700";
+	}
 
-  const className = `h-[48px] w-[240px] transform rounded-[10px] text-white shadow-lg outline-none transition delay-100 ease-in-out ${styleClass} ${isDisabled ? 'disabled:pointer-events-none disabled:opacity-50' : ''}`;
+	const className = `h-[48px] w-[240px] transform rounded-[10px] text-white shadow-lg outline-none transition delay-100 ease-in-out ${styleClass} ${
+		isDisabled ? "disabled:pointer-events-none disabled:opacity-50" : ""
+	}`;
 
-  return (
-    <AriaButton
-      onPress={onClick}
-      {...rest}
-      className={className}
-      isDisabled={isDisabled}
-    >
-      {children}
-    </AriaButton>
-  );
+	return (
+		<AriaButton
+			onPress={onClick}
+			{...rest}
+			className={className}
+			isDisabled={isDisabled}
+		>
+			{children}
+		</AriaButton>
+	);
 };
-
-export default Button;
